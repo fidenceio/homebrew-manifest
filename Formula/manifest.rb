3b4b4a3c36eb087a6035af9dfd2d78df4a43c498
William Cervin <william.cervin@lamppost.ventures>
2026-05-14 16:04:46 -0500

    Update formula to v47.12.3

--- Formula/manifest.rb
+++ Formula/manifest.rb
@@ -1,8 +1,8 @@
 class Manifest < Formula
   desc "A powerful CLI tool for managing manifest files, versioning, and repository operations with trusted timestamp verification"
   homepage "https://github.com/fidenceio/manifest.cli"
-  url "https://github.com/fidenceio/manifest.cli/archive/refs/tags/v47.12.2.tar.gz"
-  sha256 "716e9d105fc9035291ca320152615bd487ac191cc3998281ca57c5b0d8f610ec"
+  url "https://github.com/fidenceio/manifest.cli/archive/refs/tags/v47.12.3.tar.gz"
+  sha256 "90291274d33f9a5ba2dfd4a0f4f31fbdb8eda823903efec6203dbdaabdd12d37"
   license "MIT"
   head "https://github.com/fidenceio/manifest.cli.git", branch: "main"
 
@@ -118,8 +118,11 @@ class Manifest < Formula
       To always get the latest version:
         brew update && brew upgrade manifest
 
-      To uninstall cleanly (removes config and env vars too):
+      To preview a clean uninstall:
         manifest uninstall
+
+      To uninstall cleanly (removes config and env vars too):
+        manifest uninstall -y
 
       For more information, visit: https://github.com/fidenceio/manifest.cli
     EOS
